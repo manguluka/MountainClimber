0d6022cd34681f9be0c341e286a9e21e0a776da5
Tyler Manning <tylerericmanning@gmail.com>
2017-04-21 00:52:11 -0400

    Setting up Stoqscreen Sign In

--- stoqscreen/src/stoqscreen/StoqSignIn.java
+++ stoqscreen/src/stoqscreen/StoqSignIn.java
@@ -1,5 +1,11 @@
 package stoqscreen;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /* Welcome to stoqscreen; the free stock screener
  * 
  * This project is dedicated to creating a free, sleek, and intuitive 
@@ -16,17 +22,24 @@ package stoqscreen;
  *		--S I have this idea of whenever a user adds a stock to "track" the ticker
  *			is written to a file inside of the program directory. Stoqscreen then 
  *			reads the file and retrieves the data from this master "user" file.
- *
- * 
- * 
  * 
  */
 
-public class StoqSignIn {
+
+public class StoqSignIn extends Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		launch(args);
 		
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		StackPane pane = new StackPane();
+		Scene scene = new Scene(pane, 400, 500);
+		stage.setTitle("stoqscreen Sign In");
+		stage.setScene(scene); // Place the scene in the stage
+	    stage.show(); // Display the stage
 	}
 
 }
