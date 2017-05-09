eb675f34112dc661e0ead857c0b3efb748416067
Tyler Manning <tylerericmanning@gmail.com>
2017-05-09 01:23:53 -0400

    Stock Query Successfully Added to Script

--- MountainClimber/scripts/retrieve_tickers.rb
+++ MountainClimber/scripts/retrieve_tickers.rb
@@ -14,19 +14,42 @@ path = File.join(File.dirname(__FILE__), 'companylist.csv')
 
 ## RETRIEVES EVERY STOCK TICKER FROM FILE
 col_data = []
+counter = 0
 CSV.foreach(path, 'r') do |row|
-	yql << "\"" + row[0] +"\"\,"
+	#yql << "\"" + row[0] +"\"\,"
+	next if row[0] == "Symbol"
 	col_data << row[0]
 end
 ## END
+size = col_data.size / 200 + 1
+puts size
+counter = 1
+yql_query = "finance.yahoo.com/d/quotes.csv?s="
+
+for i in 0..size
+	yql_array = []
+	for j in 0..199
+		if j == 199
+			yql_query << col_data[counter].to_s + "&f=nab"
+			counter = counter + 1
+		else
+			yql_query << col_data[counter].to_s + "+"
+			counter = counter + 1
+		end
+	end
+	puts yql_query
+	puts
+    yql_query = "finance.yahoo.com/d/quotes.csv?s="
+end
+
 
 ## OUTPUTS EVERY STOCK TICKER TO NEW CSV FILE
-CSV.open("tickerlist.csv", "w") do |csv|
-	col_data.each do |i|
-		next if i == "Symbol"
-		temp_array = []
-		temp_array[0] = i
-		csv << temp_array
-	end
-end
+# CSV.open("tickerlist.csv", "w") do |csv|
+# 	col_data.each do |i|
+# 		next if i == "Symbol"
+# 		temp_array = []
+# 		temp_array[0] = i
+# 		csv << temp_array
+# 	end
+# end
 ## END
