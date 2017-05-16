649314800b064e786a8ecea0ef05643d12fb5e85
PhillipMiracle <miraclep1@nku.edu>
2017-05-16 18:51:24 -0400

    Why has God abadoned us
    ticker download still fails to work

--- MountainClimber/scripts/ticker download.rb
+++ MountainClimber/scripts/ticker download.rb
@@ -3,13 +3,19 @@
 # Description: Some basic data parsing to new file from public NASDAQ DB
 # Will probably only need to run this once a week to keep an update list of all tickers on the stock exchanges
 
-require "open-uri"
+require 'net/http'
+require 'open-uri'
+require 'openssl'
 output = File.open('output.txt','r')
-outputNew = File.open('outputNew.txt','r')
+outputNew = File.open('outputNew.txt','w')
 
 
 output.each do |row|
-	uri = URI.parse(row.chomp)
- 	contents = open(uri,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}) { |f| f.read}
-	outputNew << contents
+	uri = URI(row)
+	http = Net::HTTP.new(uri.host, uri.port)
+	http.use_ssl = true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	request = Net::HTTP::Get.new(uri.request_uri)
+	response = http.request(request)
+	outputNew << response
 end
