7b220d51f0b509b844bb207bedd616387f489acf
Tyler Manning <tylerericmanning@gmail.com>
2017-05-17 22:40:37 -0400

    Added WGET

--- MountainClimber/scripts/ticker download.rb
+++ MountainClimber/scripts/ticker download.rb
@@ -3,6 +3,12 @@
 # Description: Some basic data parsing to new file from public NASDAQ DB
 # Will probably only need to run this once a week to keep an update list of all tickers on the stock exchanges
 
+# TODO: For some reaon the CSV files are formatted differently toward the end!
+# THIS LINK https://support.klipfolio.com/hc/en-us/articles/215546368-Use-Yahoo-Finance-as-a-data-source-
+# 	WORKS WITH NYSE...for some reason
+
+# Tyler: http://www.alphavantage.co/support/#api-key
+
 require 'net/http'
 require 'open-uri'
 require 'openssl'
@@ -19,9 +25,13 @@ outputNew = File.open('outputNew.txt','w')
 # 	response = http.request(request)
 # 	outputNew << response
 # end
-url = ""
 
+url = ""
+counter = 0
 output.each do |row|
-	`wget -O "test.csv" "#{row}"`
+	filename = "test" + counter.to_s + ".csv"
+	`wget -O "#{filename}" "#{row}"`
+	counter = counter + 1
+	sleep 1
 end
 
